4c59c764543795dae2e1676364126d7ccd96c545
Lenny <lenny@itslennysfault.com>
2015-03-13 13:16:22 -0700

    whitespace

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@
 PROJECT_NAME=angular-slidezilla
 
 uglifyjs ./src/${PROJECT_NAME}.js --comments --source-map ./dist/${PROJECT_NAME}.min.js.map --source-map-url ${PROJECT_NAME}.min.js.map --output ./dist/${PROJECT_NAME}.min.js
+
+
